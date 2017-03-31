f9212443579829b7950dc7af8a66d728c3b36d20
Krzysztof Dębczyński <kdebczynski@localhost.localdomain>
2017-03-31 14:07:28 +0200

    Update to angular 4; css for mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { WelcomeComponent }     from './welcome/welcome.component';
 import { TestComponent}         from './test/test.component';
-import { ResultComponent}         from './result/result.component';
+import { ResultComponent}       from './result/result.component';
 
 
 const routes: Routes = [
